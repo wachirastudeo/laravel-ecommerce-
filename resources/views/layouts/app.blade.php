b60c156f797f463c50c9f2d0ce5fec6af9d567ec
wachira <wachirastudeo@gmail.com>
2024-08-10 13:53:55 +0700

    add css asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,8 +27,8 @@
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Allura&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/swiper.min.css') }} " type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css" type="text/css') }} " />
-    <link rel="stylesheet" href="{{ asset('assets/css/custom.css" type="text/css ') }} " />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css " />
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" type="text/css " />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
